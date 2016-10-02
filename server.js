7d873723775ff7de741cc5352a0ee2b272416a21
avtyul <avtyul@github.com>
2016-10-02 03:47:20 +0300

    server

--- server.js
+++ server.js
@@ -1,44 +1,13 @@
 let express = require('express');
 let parser = require('body-parser');
 let app = express();
-let technoDoc = require('techno-gendoc');
 let path = require('path');
 
-let technolibs = require('technolibs');
-let emailObj = {};
-
 app.use('/', express.static('public'));
-technoDoc.generate(require('./api'), 'public');
 
 app.use(parser.json());
 app.use('/libs', express.static('node_modules'));
 
-app.post('/api/messages', (req, res) => {
-	technolibs.publish(req.body).then(body => res.json(req.body));
-});
-
-app.get('/api/messages', function (req, res) {
-	res.send([
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message')),
-		technoDoc.mock(require('./api/scheme/Message'))
-	])});
-
-app.post('/users', (req, res) => {
-    console.log(req.body);
-    res.send(String(emailCount(req.body.email)));
-});
-
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`App started on port ${process.env.PORT || 3000}`);
 });
-
-function emailCount(email) {
-  if (emailObj[email] >= 1) {
-    emailObj[email]++;
-  }  else {
-    emailObj[email] = 1;
-  }
-  return emailObj[email];
-}
